30966b9263af2843892bcca7ca2eb9b7d467a36d
wiz-31218 <wiz31218@adobe.com>
2020-07-28 19:15:33 +0530

    Added bootstrap

--- js/home.js
+++ js/home.js
@@ -15,3 +15,24 @@ $(document).ready(() => {
         $('.videoModal').hide();
     })
 })
+
+// .modal-backdrop classes
+
+$(".modal-transparent").on('show.bs.modal', function () {
+    setTimeout( function() {
+      $(".modal-backdrop").addClass("modal-backdrop-transparent");
+    }, 0);
+  });
+  $(".modal-transparent").on('hidden.bs.modal', function () {
+    $(".modal-backdrop").addClass("modal-backdrop-transparent");
+  });
+  
+  $(".modal-fullscreen").on('show.bs.modal', function () {
+    setTimeout( function() {
+      $(".modal-backdrop").addClass("modal-backdrop-fullscreen");
+    }, 0);
+  });
+  $(".modal-fullscreen").on('hidden.bs.modal', function () {
+    $(".modal-backdrop").addClass("modal-backdrop-fullscreen");
+  });
+  
