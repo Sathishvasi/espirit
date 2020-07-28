1e3424f90f5681d57faf6dd2b0b4bfdcb2bd540e
sathishvasi <sathish@wizroots.com>
2020-07-28 14:13:19 +0530

    Carousel changes added

--- js/home.js
+++ js/home.js
@@ -2,5 +2,11 @@ $(document).ready(() => {
     $('#videoPlay').click(() => {
         var video = document.getElementById("myVideo");
         video.play()
-    })
+    });
+
+    $('.movie-carousel').slick({
+        initialSlide: 6,
+        slidesToShow: 6,
+        slidesToScroll: 6,
+    });
 })
